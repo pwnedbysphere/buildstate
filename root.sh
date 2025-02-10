24c10a95e07382bb40813cd84d1e036027507bdb
pwnedbysphere <sixseveneight777@proton.me>
2025-02-10 11:04:17 +0200

    Update root.sh

--- root.sh
+++ root.sh
@@ -41,22 +41,22 @@ esac
 
 if [ ! -e $ROOTFS_DIR/.installed ]; then
   mkdir $ROOTFS_DIR/usr/local/bin -p
-  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
+  wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/unroot "https://github.com/pwnedbysphere/buildstate/raw/refs/heads/main/unroot-${ARCH}"
 
-  while [ ! -s "$ROOTFS_DIR/usr/local/bin/proot" ]; do
-    rm $ROOTFS_DIR/usr/local/bin/proot -rf
-    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/proot "https://raw.githubusercontent.com/foxytouxxx/freeroot/main/proot-${ARCH}"
+  while [ ! -s "$ROOTFS_DIR/usr/local/bin/unroot" ]; do
+    rm $ROOTFS_DIR/usr/local/bin/unroot -rf
+    wget --tries=$max_retries --timeout=$timeout --no-hsts -O $ROOTFS_DIR/usr/local/bin/unroot "https://github.com/pwnedbysphere/buildstate/raw/refs/heads/main/unroot-${ARCH}"
 
-    if [ -s "$ROOTFS_DIR/usr/local/bin/proot" ]; then
-      chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    if [ -s "$ROOTFS_DIR/usr/local/bin/unroot" ]; then
+      chmod 755 $ROOTFS_DIR/usr/local/bin/unroot
       break
     fi
 
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+    chmod 755 $ROOTFS_DIR/usr/local/bin/unroot
     sleep 1
   done
 
-  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+  chmod 755 $ROOTFS_DIR/usr/local/bin/unroot
 fi
 
 if [ ! -e $ROOTFS_DIR/.installed ]; then
@@ -79,6 +79,6 @@ display_gg() {
 clear
 display_gg
 
-$ROOTFS_DIR/usr/local/bin/proot \
+$ROOTFS_DIR/usr/local/bin/unroot \
   --rootfs="${ROOTFS_DIR}" \
   -0 -w "/root" -b /dev -b /sys -b /proc -b /etc/resolv.conf --kill-on-exit
